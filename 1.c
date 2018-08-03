1c8a3f385b45c954a736fdd040d0dc2e796b789b
Your Name <you@example.com>
2018-08-03 16:20:46 +0530

    for test

---
+++ 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    struct emp
+    {
+        char name[20];
+        float sal;
+    };
+    struct emp e[10];
+    int i;
+    for(i=0; i<=9; i++){
+        scanf("%s %f", e[i].name, &e[i].sal);
+	printf("%s \t%f ",e[i].name,e[i].sal);
+	}
+    return 0;
+}
